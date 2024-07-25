5ea8639c4b7c5ef8f6dfb1e1101745ca86f76ce8
Jayanta <jayantakgope@gmail.com>
2024-07-25 16:02:53 +0530

    Practice initial commit

---
+++ JavaPractice1/src/com/practice/apps/CalculatorApp.java
@@ -0,0 +1,20 @@
+package com.practice.apps;
+
+public class CalculatorApp {
+
+	// to add
+
+	// to substract
+
+	// to multiple
+
+	// to divide
+
+	
+	
+	
+//	TODO: 
+	// to do a normal simplification
+	// 2+6/5-23*6
+
+}
